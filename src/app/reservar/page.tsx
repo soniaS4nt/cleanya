0fa4db67ac194539b8d1a91f0793cc259ee74912
“sonia” <soniachaparro21@gmail.com>
2024-02-19 18:56:39 -0300

    add reservar route

--- src/app/reservar/page.tsx
+++ src/app/reservar/page.tsx
@@ -1,17 +1,42 @@
 'use client'
 import React, { useState } from 'react'
 import { DatePickerHero } from '@/components/calendars'
-
+import MultiSelectComponent from '@/components/multiSelect'
+import dayjs from 'dayjs'
+import { DatePickerValue } from '@tremor/react'
+const DEFAULT_HOURS_AVAILABLE: { time: string }[] = [
+  { time: '09:00-10:00' },
+  { time: '11:00-12:00' },
+  { time: '13:00-14:00' },
+  { time: '15:00-16:00' },
+]
 export default function Booking() {
-  // Función para manejar el cambio de fecha
-  const handleDateChange = (fecha: any) => {
-    // Aquí podrías realizar cualquier procesamiento adicional con la fecha si lo necesitas
-    console.log(fecha)
+  const [book, setBook] = useState<string[]>([]) // Inicializar date como un array de cadenas
+  const [date, setDate] = useState<string>(dayjs(new Date()).format('DD/MM/YY'))
+
+  const handleChange = (hours: string[]) => {
+    //if(date) aqui deberia ser si la fecha esta diponible
+    setBook(hours)
+  }
+  const handleDateChange = (date: DatePickerValue) => {
+    const dateFormat = dayjs(date).format('DD/MM/YY')
+    setDate(dateFormat)
   }
 
+  const handleSubmit = () => {
+    console.log(date, book)
+  }
   return (
     <div className={'flex flex-col items-center pt-16 my-5'}>
-      <DatePickerHero onValueChange={handleDateChange} />
+      fecha: <DatePickerHero onValueChange={handleDateChange} />
+      horas disponibles:{' '}
+      <MultiSelectComponent
+        onValueChange={handleChange}
+        value={book}
+        options={DEFAULT_HOURS_AVAILABLE}
+      />
+      {/* aqui deberia ser que un boton next */}
+      <button onClick={handleSubmit}>Reservar</button>
     </div>
   )
 }
--- src/components/multiSelect.tsx
+++ src/components/multiSelect.tsx
@@ -1,18 +1,36 @@
 import React, { ChangeEvent } from 'react'
 import { MultiSelect, MultiSelectItem } from '@tremor/react'
 
-type Props = {
-  onChange: (selectedValues: string[]) => void
+type Option = {
+  time: string
 }
 
-export default function MultiSelectComponent({ onChange }: Props) {
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
-    onChange([e.target.value])
+type Props = {
+  value: string[]
+  onValueChange: (value: string[]) => void
+  options: Option[] // Especificar el tipo de options como un array de objetos Option
+}
+
+export default function MultiSelectComponent({
+  value,
+  onValueChange,
+  options,
+}: Props) {
+  const handleChange = (newValue: string[]) => {
+    onValueChange(newValue) // Pasar el nuevo valor al prop onValueChange
+  }
+
   return (
-    <MultiSelect className="mx-auto max-w-md" onChange={handleChange}>
-      <MultiSelectItem value="1">Option 1</MultiSelectItem>
-      <MultiSelectItem value="2">Option 2</MultiSelectItem>
-      <MultiSelectItem value="3">Option 3</MultiSelectItem>
+    <MultiSelect
+      className="mx-auto max-w-md"
+      onValueChange={handleChange}
+      value={value}
+    >
+      {options.map((hours, index) => (
+        <MultiSelectItem key={index} value={hours.time}>
+          {hours.time}
+        </MultiSelectItem>
+      ))}
     </MultiSelect>
   )
 }

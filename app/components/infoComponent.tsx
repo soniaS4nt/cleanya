3259e0a0847d33fda56085aad217c0c5515406a5
“sonia” <soniachaparro21@gmail.com>
2024-02-22 13:34:43 -0300

    add types

--- app/components/infoComponent.tsx
+++ app/components/infoComponent.tsx
@@ -1,28 +1,33 @@
 'use client'
-import { Divider } from '@tremor/react'
 import React, { useState } from 'react'
-type rectangleType = {
+import { Divider } from '@tremor/react'
+
+type RectangleType = {
   id: number | null
   name: string
 }
-// Datos de los rectángulos
 
-export default function InfoComponet({ rectangles, title }: any) {
-  const [selected, setSelected] = useState<null | rectangleType>(null)
+type Props = {
+  title: string
+  rectangles: RectangleType[]
+}
+
+export default function InfoComponet({ rectangles, title }: Props) {
+  const [selected, setSelected] = useState<null | RectangleType>(null)
 
   return (
     <div className="m-2">
       <small className="font-semibold text-gray-400">{title}</small>
-      <div className="grid grid-cols-3 sm:grid-cols-4  gap-3">
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-3">
         {rectangles?.map((rectangle) => (
           <div
             key={rectangle.id}
             className={`relative rounded-lg border border-gray-300 overflow-hidden shadow-lg cursor-pointer transition duration-300 ${
-              selected === rectangle.id
+              selected === rectangle
                 ? 'ring-2 ring-blue-500'
                 : 'ring-transparent'
             }`}
-            onClick={() => setSelected(rectangle.id)}
+            onClick={() => setSelected(rectangle)}
           >
             <div className="flex items-center justify-center h-full">
               <p className="p-2 text-xs md:text-base">{rectangle.name}</p>
@@ -34,4 +39,3 @@ export default function InfoComponet({ rectangles, title }: any) {
     </div>
   )
 }
-/* `p-4 rounded-lg shadow-lg cursor-pointer transition duration-300  */

74a88ed68a822edcebaf90c044f3a30dd98d15bf
“sonia” <soniachaparro21@gmail.com>
2024-02-24 13:06:57 -0300

    add var to nextConf

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   if (!API_URL) {
     return null
   }
+  console.log({ 'esta es la varEnt': process.env.NEXT_PUBLIC_BASE_API_URL })
+
   return (
     <main className="flex flex-col items-center justify-center mx-auto px-6 lg:px-24 ">
       <HeroSection />

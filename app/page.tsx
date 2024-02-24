b7f3e753f95fccd21d6f6aa86ee13fcb2bc72905
“sonia” <soniachaparro21@gmail.com>
2024-02-24 12:00:07 -0300

    add conditional

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import HeroSection from '@/components/heroSection'
 
 export default function Home() {
+  if (!process.env.NEXT_PUBLIC_BASE_API_URL) {
+    return null
+  }
   return (
     <main className="flex flex-col items-center justify-center mx-auto px-6 lg:px-24 ">
       <HeroSection />

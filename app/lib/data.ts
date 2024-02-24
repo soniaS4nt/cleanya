0597818fd408da0b28fbd9b29e4392bdb346a90d
“sonia” <soniachaparro21@gmail.com>
2024-02-24 11:42:43 -0300

    change env

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,9 +2,12 @@ import { revalidatePath, unstable_cache } from 'next/cache'
 
 export async function getData() {
   try {
-    const res = await fetch(`${process.env.PATH_URL}/api/horasDisponibles `, {
-      cache: 'no-store', // next: { revalidate: 1000 }, sacar era para refrescar la dsata de la bd
-    })
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/horasDisponibles `,
+      {
+        cache: 'no-store', // next: { revalidate: 1000 }, sacar era para refrescar la dsata de la bd
+      }
+    )
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }

e908c11b4d346fd213de7e81032571adf596fce5
“sonia” <soniachaparro21@gmail.com>
2024-02-24 17:00:52 -0300

    add dynamic

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,6 @@
 import { API_URL } from './constants'
 
+export const dynamic = 'force-dynamic'
 export async function getData() {
   try {
     const res = await fetch(`${API_URL}/api/horasDisponibles `, {

95a06ddc21b4e6f139476f4be5a6f230fd1f9a01
“sonia” <soniachaparro21@gmail.com>
2024-02-27 19:57:22 -0300

    add payment section

---
+++ app/components/pago-form.tsx
@@ -0,0 +1,39 @@
+import { TextInput } from '@tremor/react'
+// SDK de Mercado Pago
+import { MercadoPagoConfig, Preference } from 'mercadopago'
+import { redirect } from 'next/navigation'
+
+// Agrega credenciales
+const client = new MercadoPagoConfig({
+  accessToken: process.env.YOUR_ACCESS_TOKEN!,
+})
+
+export default function PagoForm() {
+  async function pagar(formData: FormData) {
+    'use server'
+    const preference = await new Preference(client).create({
+      body: {
+        items: [
+          {
+            id: 'servicio',
+            title: formData.get('title') as string,
+            quantity: 1,
+            unit_price: Number(formData.get('total')),
+          },
+        ],
+      },
+    })
+    redirect(preference.sandbox_init_point!)
+  }
+  return (
+    <form action={pagar}>
+      <div className=" rounded-lg shadow-lg">
+        <TextInput placeholder="detalle del servicio" name="title" />
+      </div>
+      <div className=" rounded-lg shadow-lg">
+        <TextInput placeholder="" name="total" />
+      </div>
+      <button type="submit"> pagar</button>
+    </form>
+  )
+}

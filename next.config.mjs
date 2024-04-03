ed7f5c90bf8718155f5f889fa45e26f8d18d0b30
“sonia” <soniachaparro21@gmail.com>
2024-04-03 18:51:34 -0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  async headers() {
-    return [
-      {
-        source: '/api/:path*', // Especifica la ruta o patrón de ruta para la cual aplicar estas cabeceras CORS
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://sandbox.mercadopago.cl', // Dominio de la API de Mercado Pago en el entorno de pruebas (sandbox)
-          },
-          // Otros encabezados CORS...
-        ],
-      },
-    ]
-=======
   webpack: function (config) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
@@ -32,7 +17,6 @@ const nextConfig = {
     }
 
     return config
->>>>>>> 3b77d0240d815e5f157b67090acc0862df238e99
   },
 }
 
